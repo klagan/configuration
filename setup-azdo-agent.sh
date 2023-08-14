035066ce201e46daf5dca7cf591ff54c94ae93fb
Kam Lagan <github@lagan.me>
2023-08-14 10:21:17 +0100

    add docker install

--- setup-azdo-agent.sh
+++ setup-azdo-agent.sh
@@ -68,11 +68,24 @@ AZ_REPO=$(lsb_release -cs)
 echo "deb [arch=`dpkg --print-architecture` signed-by=/etc/apt/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" |
     sudo tee /etc/apt/sources.list.d/azure-cli.list
 
-
 sudo apt-get update
 sudo apt-get install azure-cli
 
-# install Azure CLI - Ends
+# install docker
+# https://docs.docker.com/engine/install/ubuntu/
+
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get update
+sudo chmod 666 /var/run/docker.sock
+sudo systemctl restart docker
 
 
 # install PowerShell
